1cfd1ff9cbac94e57b7038f75f2924de560beb01
Ekam Preet Singh <ekampreet.singh6862@gmail.com>
2022-02-10 11:55:25 +0530

    Day 3/90 Practice

---
+++ 01_Array/Q11.cpp
@@ -0,0 +1,161 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+#define pb push_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll, ll>
+#define ump unordered_map
+#define mp make_pair
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll, vi, greater<ll>>
+#define all(n) n.begin(), n.end()
+#define ff first
+#define ss second
+#define mid(l, r) (l + (r - l) / 2)
+#define bitc(n) __builtin_popcount(n)
+#define loop(i, a, b) for (int i = (a); i <= (b); i++)
+#define looprev(i, a, b) for (int i = (a); i >= (b); i--)
+#define iter(container, it) for (__typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define log(args...)                             \
+    {                                            \
+        string _s = #args;                       \
+        replace(_s.begin(), _s.end(), ',', ' '); \
+        stringstream _ss(_s);                    \
+        istream_iterator<string> _it(_ss);       \
+        err(_it, args);                          \
+    }
+#define logarr(arr, a, b)            \
+    for (int z = (a); z <= (b); z++) \
+        cout << (arr[z]) << " ";     \
+    cout << endl;
+template <typename T>
+T gcd(T a, T b)
+{
+    if (a % b)
+        return gcd(b, a % b);
+    return b;
+}
+template <typename T>
+T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
+vs tokenizer(string str, char ch)
+{
+    std::istringstream var((str));
+    vs v;
+    string t;
+    while (getline((var), t, (ch)))
+    {
+        v.pb(t);
+    }
+    return v;
+}
+
+void err(istream_iterator<string> it) {}
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+    cout << *it << " = " << a << endl;
+    err(++it, args...);
+}
+// typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
+
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+/*
+Q11.) find duplicate in an array of N + 1 Integers
+
+INPUT:
+3
+
+5
+1 3 4 2 2
+
+8
+1 5 7 9 3 5 8 5
+
+5
+3 2 3 4 2
+
+OUTPUT:
+Case #1: 2
+Case #2: 5
+Case #3: 3
+
+*/
+
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+        int duplicate = -1;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            // Making curr positive, as index can't be negative
+            int cur = abs(nums[i]);
+            // If number is negative, It has been passed before
+            if (nums[cur] < 0)
+            {
+                // Found the duplicate number
+                duplicate = cur;
+                break;
+            }
+            // Making every element negative
+            nums[cur] *= -1;
+        }
+        return duplicate;
+    }
+};
+
+void solve()
+{
+
+    int n;
+    cin >> n;
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    Solution ob;
+    cout << ob.findDuplicate(v) << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+#ifndef ONLINE_JUDGE
+    clock_t begin = clock();
+    file_i_o();
+#endif
+
+    ll t = 1;
+    ll case_num = 1;
+    cin >> t;
+    while (t--)
+    {
+        cout << "Case #" << case_num++ << ": ";
+        solve();
+    }
+
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted In: " << double(end - begin) / CLOCKS_PER_SEC * 1000 << " ms";
+#endif
+    return 0;
+}
---
+++ Algorithms/Merge_Sort.cpp
@@ -0,0 +1,225 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+#define pb push_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll, ll>
+#define ump unordered_map
+#define mp make_pair
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll, vi, greater<ll>>
+#define all(n) n.begin(), n.end()
+#define ff first
+#define ss second
+#define mid(l, r) (l + (r - l) / 2)
+#define bitc(n) __builtin_popcount(n)
+#define loop(i, a, b) for (int i = (a); i <= (b); i++)
+#define looprev(i, a, b) for (int i = (a); i >= (b); i--)
+#define iter(container, it) for (__typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define log(args...)                             \
+    {                                            \
+        string _s = #args;                       \
+        replace(_s.begin(), _s.end(), ',', ' '); \
+        stringstream _ss(_s);                    \
+        istream_iterator<string> _it(_ss);       \
+        err(_it, args);                          \
+    }
+#define logarr(arr, a, b)            \
+    for (int z = (a); z <= (b); z++) \
+        cout << (arr[z]) << " ";     \
+    cout << endl;
+template <typename T>
+T gcd(T a, T b)
+{
+    if (a % b)
+        return gcd(b, a % b);
+    return b;
+}
+template <typename T>
+T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
+vs tokenizer(string str, char ch)
+{
+    std::istringstream var((str));
+    vs v;
+    string t;
+    while (getline((var), t, (ch)))
+    {
+        v.pb(t);
+    }
+    return v;
+}
+
+void err(istream_iterator<string> it) {}
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+    cout << *it << " = " << a << endl;
+    err(++it, args...);
+}
+// typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
+
+void file_i_o()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+/*
+Merge Sort
+
+Merge Sort is a Divide and Conquer algorithm.
+It divides the input array into two halves, calls itself for the two halves, and then merges the two sorted halves.
+The merge() function is used for merging two halves.
+The merge(arr, l, m, r) is a key process that assumes that arr[l..m] and arr[m+1..r] are sorted and merges the two sorted sub-arrays into one.
+
+Time Complexity: O(n*logn)
+Space Complexity: O(n)
+
+Pseudo Code
+
+MergeSort(arr[], l,  r)
+If r > l
+     1. Find the middle point to divide the array into two halves:
+             middle m = l+ (r-l)/2
+     2. Call mergeSort for first half:
+             Call mergeSort(arr, l, m)
+     3. Call mergeSort for second half:
+             Call mergeSort(arr, m+1, r)
+     4. Merge the two halves sorted in step 2 and 3:
+             Call merge(arr, l, m, r)
+
+*/
+
+class Solution
+{
+public:
+    void merge(int arr[], int l, int m, int r)
+    {
+        int i, j, k;
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        /* create temp arrays */
+        int L[n1], R[n2];
+
+        /* Copy data to temp arrays L[] and R[] */
+        for (i = 0; i < n1; i++)
+        {
+            L[i] = arr[l + i];
+        }
+
+        for (j = 0; j < n2; j++)
+        {
+            R[j] = arr[m + 1 + j];
+        }
+
+        /* Merge the temp arrays back into arr[l..r]*/
+        i = 0; // Initial index of first subarray
+        j = 0; // Initial index of second subarray
+        k = l; // Initial index of merged subarray
+
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy the remaining elements of L[], if there are any */
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy the remaining elements of R[], if there are any */
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    void mergeSort(int arr[], int l, int r)
+    {
+        if (l < r)
+        {
+            // Same as (l+r)/2, but avoids overflow for
+            // large l and h
+            int m = l + (r - l) / 2;
+
+            // Sort first and second halves
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+
+            merge(arr, l, m, r);
+        }
+    }
+};
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void solve()
+{
+    int i, n;
+    cin >> n;
+    int arr[n + 1];
+    for (i = 0; i < n; i++)
+        cin >> arr[i];
+
+    Solution ob;
+    ob.mergeSort(arr, 0, n - 1);
+    printArray(arr, n);
+}
+
+int main(int argc, char const *argv[])
+{
+#ifndef ONLINE_JUDGE
+    clock_t begin = clock();
+    file_i_o();
+#endif
+
+    ll t = 1;
+    // ll case_num = 1;
+    cin >> t;
+    while (t--)
+    {
+        // cout << "Case #" << case_num++ << ": ";
+        solve();
+    }
+
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted In: " << double(end - begin) / CLOCKS_PER_SEC * 1000 << " ms";
+#endif
+    return 0;
+}
