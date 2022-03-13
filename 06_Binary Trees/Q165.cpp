73cd1bae845e02223d9938f7533210fe782e41bf
Ekam Preet Singh <ekampreet.singh6862@gmail.com>
2022-03-13 10:22:12 +0530

    Day 33/90 Practice

---
+++ 06_Binary Trees/Q165.cpp
@@ -0,0 +1,253 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+#define pb push_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll, ll>
+#define ump unordered_map
+#define mp make_pair
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll, vi, greater<ll>>
+#define all(n) n.begin(), n.end()
+#define ff first
+#define ss second
+#define mid(l, r) (l + (r - l) / 2)
+#define bitc(n) __builtin_popcount(n)
+#define loop(i, a, b) for (int i = (a); i <= (b); i++)
+#define looprev(i, a, b) for (int i = (a); i >= (b); i--)
+#define iter(container, it) for (__typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define log(args...)                             \
+    {                                            \
+        string _s = #args;                       \
+        replace(_s.begin(), _s.end(), ',', ' '); \
+        stringstream _ss(_s);                    \
+        istream_iterator<string> _it(_ss);       \
+        err(_it, args);                          \
+    }
+#define logarr(arr, a, b)            \
+    for (int z = (a); z <= (b); z++) \
+        cout << (arr[z]) << " ";     \
+    cout << endl;
+template <typename T>
+T gcd(T a, T b)
+{
+    if (a % b)
+        return gcd(b, a % b);
+    return b;
+}
+template <typename T>
+T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
+vs tokenizer(string str, char ch)
+{
+    std::istringstream var((str));
+    vs v;
+    string t;
+    while (getline((var), t, (ch)))
+    {
+        v.pb(t);
+    }
+    return v;
+}
+
+void err(istream_iterator<string> it) {}
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+    cout << *it << " = " << a << endl;
+    err(++it, args...);
+}
+// typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
+
+void file_i_o()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+/*
+Q165.) Mirror of a tree
+
+INPUT:
+4
+
+1 3 2
+
+5 5 N 4 10 N 8 5 N 8 8 N 6
+
+10 20 30 40 60 N N
+
+5 3 6 2 4 N N
+
+OUTPUT:
+Case #1:
+Inorder of original tree: 3 1 2
+Inorder of Mirror tree: 2 1 3
+Case #2:
+Inorder of original tree: 4 8 8 8 5 5 6 10 5
+Inorder of Mirror tree: 5 10 6 5 5 8 8 8 4
+Case #3:
+Inorder of original tree: 40 20 60 10 30
+Inorder of Mirror tree: 30 10 60 20 40
+Case #4: 
+Inorder of original tree: 2 3 4 5 6 
+Inorder of Mirror tree: 6 5 4 3 2 
+
+*/
+
+/* A binary tree node has val, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int val;
+    struct Node *left;
+    struct Node *right;
+};
+Node *newNode(int val)
+{
+    Node *temp = new Node;
+    temp->val = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+Node *buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size())
+    {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N")
+        {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N")
+        {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+Node *mirrorTree(Node *&root)
+{
+    if (root == NULL)
+    {
+        return root;
+    }
+    Node *t = root->left;
+    root->left = root->right;
+    root->right = t;
+
+    if (root->left)
+    {
+        mirrorTree(root->left);
+    }
+    if (root->right)
+    {
+        mirrorTree(root->right);
+    }
+
+    return root;
+}
+
+void inorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->val << " ";
+    inorder(root->right);
+}
+
+void solve()
+{
+    string s;
+    getline(cin, s);
+    Node *tree = buildTree(s);
+
+    cout << "\nInorder of original tree: ";
+    inorder(tree);
+
+    mirrorTree(tree);
+
+    cout << "\nInorder of Mirror tree: ";
+    inorder(tree);
+}
+
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    file_i_o();
+
+    ll t = 1;
+    ll case_num = 1;
+    // cin >> t;
+    while (t--)
+    {
+        cout << "Case #" << case_num++ << ": ";
+        solve();
+    }
+
+    return 0;
+}
