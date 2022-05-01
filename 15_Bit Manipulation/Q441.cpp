d69efd096a6cc2646f6dae6f35521d425c705031
Ekam Preet Singh <ekampreet.singh6862@gmail.com>
2022-05-01 10:19:31 +0530

    Day 89/90 Practice

---
+++ 15_Bit Manipulation/Q441.cpp
@@ -0,0 +1,171 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+#define pb push_back
+#define vi vector<ll>
+#define vs vector<string>
+#define pii pair<ll, ll>
+#define ump unordered_map
+#define mp make_pair
+#define pq_max priority_queue<ll>
+#define pq_min priority_queue<ll, vi, greater<ll>>
+#define all(n) n.begin(), n.end()
+#define ff first
+#define ss second
+#define mid(l, r) (l + (r - l) / 2)
+#define bitc(n) __builtin_popcount(n)
+#define loop(i, a, b) for (int i = (a); i <= (b); i++)
+#define looprev(i, a, b) for (int i = (a); i >= (b); i--)
+#define iter(container, it) for (__typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define log(args...)                             \
+    {                                            \
+        string _s = #args;                       \
+        replace(_s.begin(), _s.end(), ',', ' '); \
+        stringstream _ss(_s);                    \
+        istream_iterator<string> _it(_ss);       \
+        err(_it, args);                          \
+    }
+#define logarr(arr, a, b)            \
+    for (int z = (a); z <= (b); z++) \
+        cout << (arr[z]) << " ";     \
+    cout << endl;
+template <typename T>
+T gcd(T a, T b)
+{
+    if (a % b)
+        return gcd(b, a % b);
+    return b;
+}
+template <typename T>
+T lcm(T a, T b) { return (a * (b / gcd(a, b))); }
+vs tokenizer(string str, char ch)
+{
+    std::istringstream var((str));
+    vs v;
+    string t;
+    while (getline((var), t, (ch)))
+    {
+        v.pb(t);
+    }
+    return v;
+}
+
+void err(istream_iterator<string> it) {}
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+    cout << *it << " = " << a << endl;
+    err(++it, args...);
+}
+// typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+// typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
+
+void file_i_o()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+/*
+Q441.) Find position of the only set bit
+
+INPUT:
+6
+
+4
+
+17
+
+1
+
+98
+
+2
+
+5
+
+OUTPUT:
+Case #1: 3
+Case #2: -1
+Case #3: 1
+Case #4: -1
+Case #5: 2
+Case #6: -1
+
+*/
+
+class Solution
+{
+public:
+    // A helper method which returns 1 if n is a power of 2 else returns 0.
+    int isPowerOfTwo(int n)
+    {
+        if (n && (!(n & (n - 1))))
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    int findPosition(int N)
+    {
+
+        if (!isPowerOfTwo(N))
+            return -1;
+
+        int i = 1, pos = 1;
+
+        // Iterate through bits of n till we find a set bit
+        // i&n will be non-zero only when 'i' and 'n' have a set bit
+        // at same position
+        while (!(i & N))
+        {
+            // Unset current bit and set the next bit in 'i'
+            i = i << 1;
+
+            // increment position
+            ++pos;
+        }
+
+        return pos;
+    }
+};
+
+void solve()
+{
+    int N;
+
+    cin >> N;
+
+    Solution ob;
+    cout << ob.findPosition(N) << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    file_i_o();
+
+    ll t = 1;
+    ll case_num = 1;
+    cin >> t;
+    while (t--)
+    {
+        cout << "Case #" << case_num++ << ": ";
+        solve();
+    }
+
+    return 0;
+}
